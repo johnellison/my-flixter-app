3f55e9833c39cf915b2f3ad8bedca4e336099942
John Ellison <info@johnwebsites.com>
2015-02-12 11:01:19 -0700

    add create and show actions, set up basic show view

--- app/controllers/teacher/courses_controller.rb
+++ app/controllers/teacher/courses_controller.rb
@@ -4,4 +4,20 @@ class Teacher::CoursesController < ApplicationController
     @course = Course.new
   end
 
+  def create
+    @course = current_user.courses.create(course_params)
+    redirect_to teacher_course_path(@course)
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  private
+    
+  def course_params
+    params.require(:course).permit(:title, :description, :cost)
+  end
+
+
 end
