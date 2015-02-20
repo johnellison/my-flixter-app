03798793b634d9d1eed890bcd947974483aec705
John Ellison <info@johnwebsites.com>
2015-02-20 09:46:34 -0700

    update to save to file on localhost env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,17 @@
-CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV.fetch('AWS_BUCKET')
-  config.aws_acl    = :public_read
+if Rails.env.development? or Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage    = :aws
+    config.aws_bucket = ENV.fetch('AWS_BUCKET')
+    config.aws_acl    = :public_read
 
-  config.aws_credentials = {
-    access_key_id:     ENV.fetch('AWS_ACCESS_KEY'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
-  }
+    config.aws_credentials = {
+      access_key_id:     ENV.fetch('AWS_ACCESS_KEY'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+    }
+  end
 end
