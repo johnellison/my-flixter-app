4a5b6db600c93d461fbd229326e59c703d5ceeb6
John Ellison <info@johnwebsites.com>
2015-02-24 11:01:22 -0700

    update test; still failing

--- test/controllers/enrollments_controller_test.rb
+++ test/controllers/enrollments_controller_test.rb
@@ -3,19 +3,18 @@ require 'test_helper'
 class EnrollmentsControllerTest < ActionController::TestCase
   test "create enrollment" do
     user = FactoryGirl.create(:user)
-    sign_in user
-
     course = FactoryGirl.create(:course)
 
     assert_difference 'user.enrollments.count' do
       post :create, { 
         enrollment: {
-          current_course: course
+          user: user,
+          course: course
         }
       }
     end
 
-    # assert_redirected_to course_path(course)
-    # assert_equal 1, user.enrollments.count
+    assert_redirected_to course_path(course)
+    assert_equal 1, user.enrollments.count
   end
 end
