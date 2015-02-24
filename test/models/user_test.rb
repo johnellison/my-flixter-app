4c2ac81c61c23449bde6178cae8fb51c9c8f63f1
John Ellison <info@johnwebsites.com>
2015-02-23 18:17:17 -0700

    refactor

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "user course enrollment" do
+  test "test if user is enrolled in course" do
     user = FactoryGirl.build(:user)
     course = FactoryGirl.build(:course)
     enrollment = FactoryGirl.build(:enrollment)
