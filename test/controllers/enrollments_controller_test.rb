e3c858cf0983d0f11c41dc66d745aa97790fcec7
John Ellison <info@johnwebsites.com>
2015-02-24 10:54:31 -0700

    add initial test; failing

--- test/controllers/enrollments_controller_test.rb
+++ test/controllers/enrollments_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class EnrollmentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create enrollment" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+
+    course = FactoryGirl.create(:course)
+
+    assert_difference 'user.enrollments.count' do
+      post :create, { 
+        enrollment: {
+          current_course: course
+        }
+      }
+    end
+
+    # assert_redirected_to course_path(course)
+    # assert_equal 1, user.enrollments.count
+  end
 end
