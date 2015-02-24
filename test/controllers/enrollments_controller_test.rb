16d2dd7718bc4244ec536a157137f69de57bb20a
John Ellison <info@johnwebsites.com>
2015-02-24 11:25:39 -0700

    update test to pass course_id into post :create URL; passing

--- test/controllers/enrollments_controller_test.rb
+++ test/controllers/enrollments_controller_test.rb
@@ -3,15 +3,11 @@ require 'test_helper'
 class EnrollmentsControllerTest < ActionController::TestCase
   test "create enrollment" do
     user = FactoryGirl.create(:user)
+    sign_in user
     course = FactoryGirl.create(:course)
 
     assert_difference 'user.enrollments.count' do
-      post :create, { 
-        enrollment: {
-          user: user,
-          course: course
-        }
-      }
+      post :create, course_id: course.id
     end
 
     assert_redirected_to course_path(course)
