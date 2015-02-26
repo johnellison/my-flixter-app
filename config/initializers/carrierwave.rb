30c88ae9545fa585142c19c74e762268272fb803
John Ellison <info@johnwebsites.com>
2015-02-26 14:01:18 -0700

    update for fog ENV syntax

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ if Rails.env.development?
   CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = false
-  end
+end
 
 if Rails.env.test?
   # auto load uploaders
@@ -27,12 +27,12 @@ end
 else
   CarrierWave.configure do |config|
     config.storage    = :aws
-    config.aws_bucket = ENV.fetch('AWS_BUCKET')
+    config.aws_bucket = ENV['AWS_BUCKET']
     config.aws_acl    = :public_read
 
     config.aws_credentials = {
-      access_key_id:     ENV.fetch('AWS_ACCESS_KEY'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+      access_key_id:     ENV['AWS_ACCESS_KEY'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
   end
 end
